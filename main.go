03d3d6f934d14780e38c75d5c0df96aba342d996
David Zhu <shaowenzhu@dal.ca>
2023-10-06 23:55:17 -0300

    feat: add upload file feature

--- main.go
+++ main.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
 	"io"
+	"log"
 	"net/http"
 	"os"
 	"path/filepath"
 	"time"
 )
+
+var root = "./files"
 
 func handleUploadFile(w http.ResponseWriter, r *http.Request) {
 	if r.Method != "POST" {
@@ -18,7 +22,8 @@ func handleUploadFile(w http.ResponseWriter, r *http.Request) {
 	var maxUploadSize int64 = 1 << 25 // 32MB
 	r.Body = http.MaxBytesReader(w, r.Body, maxUploadSize)
 	if err := r.ParseMultipartForm(maxUploadSize); err != nil {
-		http.Error(w, "Uploaded file is too big. Choose an file less than 32MB", http.StatusBadRequest)
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		log.Print(err)
 		return
 	}
 
@@ -28,15 +33,14 @@ func handleUploadFile(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer file.Close()
-	folderName := "files"
 	// Create the uploads folder if it doesn't already exist
-	err = os.MkdirAll(fmt.Sprintf("./%v", folderName), os.ModePerm)
+	err = os.MkdirAll(root, os.ModePerm)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 	// Create a new file in the uploads directory
-	dst, err := os.Create(fmt.Sprintf("./%v/%d%s", folderName, time.Now().UnixNano(), filepath.Ext(fileHeader.Filename)))
+	dst, err := os.Create(fmt.Sprintf("%v/%d%s", root, time.Now().UnixNano(), filepath.Ext(fileHeader.Filename)))
 	if err != nil {
 		http.Error(w, fmt.Errorf("failed to create dst file: %v", err).Error(), http.StatusInternalServerError)
 		return
@@ -57,29 +61,64 @@ func handleUploadFile(w http.ResponseWriter, r *http.Request) {
 	_, _ = fmt.Fprintf(w, "upload successful")
 }
 
-type file struct {
-	Path string
-	Size int
+func handleDownloadFile(w http.ResponseWriter, r *http.Request) {
+
 }
 
-func handleDownloadFile(w http.ResponseWriter, r *http.Request) {
-	files := []file{
-		{
-			Path: "hello",
-			Size: 1,
-		},
-		{
-			Path: "hihi",
-			Size: 2,
-		},
+func handleHomePage(w http.ResponseWriter, r *http.Request) {
+	if _, err := os.Stat(root); errors.Is(err, os.ErrNotExist) {
+		err := os.Mkdir(root, os.ModePerm)
+		if err != nil {
+			log.Println(err)
+		}
 	}
-	tmpl := template.Must(template.ParseFiles("template/index.template"))
-	_ = tmpl.Execute(w, files)
+	type File struct {
+		Name string
+		Path string
+		Size string
+	}
+	// get files in the specified root folder
+	files, err := os.ReadDir(root)
+	if err != nil {
+		http.Error(w, fmt.Errorf("failed to open root folder: %v", err).Error(), http.StatusInternalServerError)
+		return
+	}
+	data := make([]File, 0)
+	for _, f := range files {
+		// ignore folder
+		if f.IsDir() {
+			continue
+		}
+		info, err := f.Info()
+		if err != nil {
+			log.Printf("failed to get info of %v: %v", f.Name(), err)
+			continue
+		}
+		// size less than 100KB, displayed as KB
+		var size string
+		if info.Size() <= 102400 {
+			size = fmt.Sprintf("%.2f KB", float32(info.Size())/(1<<10))
+		} else {
+			size = fmt.Sprintf("%.2f MB", float32(info.Size())/(1<<20))
+		}
+		data = append(data, File{
+			Name: f.Name(),
+			Path: fmt.Sprintf("%v/%v", root, f.Name()),
+			Size: size,
+		})
+	}
+	//pass files' info to the HTML template
+	tmpl, err := template.ParseFiles("./template/home.html")
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Printf("failed to execute template:%v", err)
+	}
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/login", handleUploadFile)
+	mux.HandleFunc("/home", handleHomePage)
+	mux.HandleFunc("/upload", handleUploadFile)
 	mux.HandleFunc("/hi", handleDownloadFile)
 	_ = http.ListenAndServe(":8080", mux)
 }
